ff7069cd3cecb67c950bd125f4bf1b024ce1d3ba
Aleksandar Babic <contact@aleksandar-babic.com>
2024-04-04 09:39:04 +0200

    Updated test to include env var patching

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,9 @@ from src.main import main
 
 
 @patch("logging.info")
-def test_main_call_logging(mock_logging):
+def test_main_call_logging(mock_logging, monkeypatch):
+    foo_env = "test"
+    monkeypatch.setenv("FOO", foo_env)
+
     main()
-    mock_logging.assert_called_with("Hello World!")
+    mock_logging.assert_called_with(f"Hello World! foo={foo_env}")
